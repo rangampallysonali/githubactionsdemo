5c76ac45b8797ff1bba465ae03a3a71a5d7186f3
Sonali Rangampally <srangampally1@toromail.csudh.edu>
2026-04-14 21:10:18 -0700

    changes 4

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def delete_book(book_id):
     return jsonify({"message": "Book deleted"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=4000)
